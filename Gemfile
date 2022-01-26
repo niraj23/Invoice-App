f71f530ee4277ef7d8f24ad34c3d5bb379d8b72a
Niraj <niraj2323@gmail.com>
2022-01-26 15:43:47 -0500

    Completed

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'rack-cors'
 
+gem 'jwt', '~> 2.2', '>= 2.2.3'
+
 gem 'dotenv-rails', '~> 2.7', '>= 2.7.6'
 
 group :development, :test do
--- config/application.rb
+++ config/application.rb
@@ -3,15 +3,15 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
-# require "active_storage/engine"
+require "active_storage/engine"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
+require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -19,16 +19,8 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Phase4RailsPuttingItAllTogetherAuth
+module Phase4
   class Application < Rails::Application
-    # Adding cookies and session middleware
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
-
-    # Use SameSite=Strict for all cookies to help protect against CSRF
-    # https://owasp.org/www-community/SameSite
-    config.action_dispatch.cookies_same_site_protection = :strict
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
@@ -44,5 +36,10 @@ module Phase4RailsPuttingItAllTogetherAuth
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_phase4', expire_after: 20.years
+    config.middleware.insert_after(
+      ActionDispatch::Cookies, ActionDispatch::Session::CookieStore, key: '_phase4'
+    )
   end
 end
